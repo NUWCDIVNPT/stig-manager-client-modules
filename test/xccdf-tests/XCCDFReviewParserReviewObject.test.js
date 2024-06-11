afa15e4c0a55687e899829ccc774dbef9418c03f
Mathew <mferreira1264@gmail.com>
2024-06-11 13:17:07 -0400

    Fix: parsed results will now truncate values to conform to stig-manager API spec.

--- test/xccdf-tests/XCCDFReviewParserReviewObject.test.js
+++ test/xccdf-tests/XCCDFReviewParserReviewObject.test.js
@@ -1856,4 +1856,42 @@ describe('MISC. xccdf ', () => {
       expectedOveride
     )
   })
+  it('Validating that parser truncates review values to their max oas spec', async () => {
+    // values tested: ruleId
+    const importOptions = {
+      autoStatus: 'saved',
+      unreviewed: 'commented',
+      unreviewedCommented: 'informational',
+      emptyDetail: 'replace',
+      emptyComment: 'ignore',
+      allowCustom: true
+    }
+
+    const fieldSettings = {
+      detail: {
+        enabled: 'always',
+        required: 'always'
+      },
+      comment: {
+        enabled: 'findings',
+        required: 'findings'
+      }
+    }
+
+    const allowAccept = true
+
+    const filePath =
+      './WATCHER-test-files/WATCHER/xccdf/Target-Object-Long-Properties-xccdf.xml'
+
+    const review = await generateReviewObject(
+      filePath,
+      importOptions,
+      fieldSettings,
+      allowAccept
+    )
+    
+    expect(review.checklists[0].reviews[0].ruleId).to.have.lengthOf(45)
+
+    
+  })
 })
